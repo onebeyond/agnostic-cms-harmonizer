7f3adf7488967b019f4872bfa89616e51853f11a
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-14 18:11:10 +0100

    docs: autogenerate docs using typedoc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+/**
+ * Funtion to say hello to a person
+ * @param {string} name Name of the person to greet
+ * @returns void
+ */
+const hello = (name: string) => console.log(`Hello ${name}!`);
+
+export default hello;
