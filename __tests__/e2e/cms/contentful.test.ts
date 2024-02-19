8b7adc6c55c331851cfd1227c163223ce2227477
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-19 12:48:42 +0100

    test: add test to fetch content in a different locale

--- __tests__/e2e/cms/contentful.test.ts
+++ __tests__/e2e/cms/contentful.test.ts
@@ -5,7 +5,7 @@ import { Contentful } from '../../../src/cms/contentful';
 
 describe('contentful', () => {
   describe('should not fail', () => {
-    it('retrieve content', async () => {
+    it('retrieve content in default locale (en-US)', async () => {
       const contentful = new Contentful({
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
         environment: process.env.CONTENTFUL_ENVIRONMENT as string,
@@ -14,6 +14,20 @@ describe('contentful', () => {
       await contentful.initialize();
       const harmonizedData = await contentful.getEntry(
         process.env.CONTENTFUL_ENTRY as string,
+      );
+      expect(harmonizedData).toMatchSnapshot();
+    });
+
+    it('retrieve content in specific locale `es`', async () => {
+      const contentful = new Contentful({
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+        environment: process.env.CONTENTFUL_ENVIRONMENT as string,
+        space: process.env.CONTENTFUL_SPACE as string,
+      });
+      await contentful.initialize();
+      const harmonizedData = await contentful.getEntry(
+        process.env.CONTENTFUL_ENTRY as string,
+        'es',
       );
       expect(harmonizedData).toMatchSnapshot();
     });
