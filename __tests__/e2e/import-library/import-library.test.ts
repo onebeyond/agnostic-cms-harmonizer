f3821e804268b8f9bf48fd9ad4d8ae89e6c94adf
roberto <roberto.hernandez@one-beyond.com>
2024-04-30 13:35:05 +0200

    refactor: rename test name and test package json info

--- __tests__/e2e/import-library/import-library.test.ts
+++ __tests__/e2e/import-library/import-library.test.ts
@@ -1,13 +1,12 @@
 // eslint-disable-next-line import/no-unresolved
 import { ContentfulClient } from '@onebeyond/agnostic-cms-harmonizer';
 
-describe('import-library', () => {
-  it('should import the library', () => {
-    const instance = new ContentfulClient({
-      accessToken: '',
-      space: '',
-      environment: '',
-    });
-    expect(instance).toBeInstanceOf(ContentfulClient);
+it('should check the imported library is an instance of ContentfulClient', () => {
+  const instance = new ContentfulClient({
+    accessToken: '',
+    space: '',
+    environment: '',
   });
+  expect(instance).toBeInstanceOf(ContentfulClient);
 });
+describe('import-library', () => {});
